3959e145c8f3c009ebbc8c6bfed09043ed47eb08
Peter Larson <p.larson@utexas.edu>
2023-04-03 11:24:21 -0500

    Initial Commit

---
+++ TCA-Snake 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCA-Snake 2
+//
+//  Created by Peter Larson on 4/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA-Snake 2/TCA_Snake_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Snake_2App.swift
+//  TCA-Snake 2
+//
+//  Created by Peter Larson on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Snake_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
